b841869d23a00f82ec7dea0aa8ac11ee82627a4b
palmdrop <anton@exlex.se>
2021-08-21 15:44:08 +0200

    Reset default css styling

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
 
 import './index.scss';
+
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
