e8dc518cba353d1330470fb484791630f2e5aa13
palmdrop <anton@exlex.se>
2021-09-01 17:42:27 +0200

    Create working color scheme switcher

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PageRoute, useNavigation } from '../../App';
 import Bar from '../ornamental/bars/Bar';
 import Title from '../title/Title';
 
@@ -10,46 +11,65 @@ type Props = {
   secondSubtitle? : string,
   mainLevel? : number,
   subLevel? : number,
+  linkTo? : PageRoute,
   children? : React.ReactChild | React.ReactChild[] | never[]
 };
 
-const Header = ( { mainTitle, firstSubtitle, secondSubtitle, mainLevel = 1, subLevel = 5, children } : Props ) : JSX.Element => {
-  return (
-    <header className="header">
-      <div>
-        <Title 
-          level={ mainLevel }
-          text={ mainTitle }
-        />
+const Header = ( { mainTitle, firstSubtitle, secondSubtitle, mainLevel = 1, subLevel = 5, linkTo, children } : Props ) : JSX.Element => {
+  const navigateTo = useNavigation();
 
-        { ( firstSubtitle || secondSubtitle ) ?
-          <Bar
-            direction="horizontal"
-            variant="inset"
+  const handleClick = () : void => {
+    linkTo && navigateTo( linkTo );
+  };
+
+  const headerContent = (
+    <div>
+      <Title 
+        level={ mainLevel }
+        text={ mainTitle }
+      />
+
+      { ( firstSubtitle || secondSubtitle ) ?
+        <Bar
+          direction="horizontal"
+          variant="inset"
+        />
+        : null
+      }
+
+      <div>
+        { firstSubtitle ? 
+          <Title
+            level={ subLevel }
+            text={ firstSubtitle }
           />
           : null
         }
 
-        <div>
-          { firstSubtitle ? 
-            <Title
-              level={ subLevel }
-              text={ firstSubtitle }
-            />
-            : null
-          }
-
-          { secondSubtitle ? 
-            <Title
-              level={ subLevel }
-              text={ secondSubtitle }
-            />
-            : null
-          }
-        </div>
-
-        { children }
+        { secondSubtitle ? 
+          <Title
+            level={ subLevel }
+            text={ secondSubtitle }
+          />
+          : null
+        }
       </div>
+
+      { children }
+    </div>
+  );
+
+  return (
+    <header 
+      className={ `header ${ linkTo ? 'header--link' : '' }` }
+    >
+      { linkTo ? (
+          <button onClick={ handleClick }>
+            { headerContent }
+          </button>
+        ) 
+        : headerContent 
+      }
     </header>
   )
 }
