18a1aeb1761cc964235e8175bef2929a41d69458
palmdrop <anton@exlex.se>
2021-10-22 21:51:46 +0200

    Update main text

--- src/pages/main/content.ts
+++ src/pages/main/content.ts
@@ -2,7 +2,5 @@
 export const description : string[] = [
   "A personal webspace for exploring ideas and generative art.",
 
-  "By allowing the Internet to centralize on large corporate platforms, we lose individuality and autonomy.",
-
-  "This place is experimental and will always be a work in progress."
+  "This place is experimental and will always be a work in progress. None of your data is tracked."
 ];
