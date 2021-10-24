fd285840d85d19571033afe8031983b8eda50039
palmdrop <anton@exlex.se>
2021-10-24 10:26:40 +0200

    Force server to always use HTTPS

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import HttpsRedirect from 'react-https-redirect';
 import { Provider } from 'react-redux';
 import { store } from './state/store/store';
 
@@ -11,11 +10,9 @@ import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HttpsRedirect>
-      <Provider store={ store }>
-        <App />
-      </Provider>
-    </HttpsRedirect>
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
