79c0fe4e8e96ecb74f8df76112ffd7269aecaea4
palmdrop <anton@exlex.se>
2021-10-31 14:05:21 +0100

    Do I care about emailjs keys anyway?

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,12 +1,17 @@
 import emailjs from 'emailjs-com';
-import { serviceID, templateID, userID } from '../credentials';
+
+const { 
+  REACT_APP_EMAILJS_USER_ID : userID,
+  REACT_APP_EMAILJS_SERVICE_ID : serviceID,
+  REACT_APP_EMAILJS_TEMPLATE_ID : templateID,
+} = process.env;
 
 // Thanks to https://stackoverflow.com/a/201378 for regex
 const emailValidationRegex = 
   /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 // EmailJS
-emailjs.init( userID );
+emailjs.init( userID as string );
 
 export const isValidEmail = ( email : string ) => {
   return emailValidationRegex.test( email );
@@ -14,8 +19,8 @@ export const isValidEmail = ( email : string ) => {
 
 export const sendFormEmail = ( form : HTMLFormElement ) => {
   return emailjs.sendForm( 
-    serviceID,
-    templateID,
+    serviceID as string,
+    templateID as string,
     form,
     userID
   );
