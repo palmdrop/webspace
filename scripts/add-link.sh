a8de712e0b5b766263a081a1aa36a2553c8cf598
palmdrop <anton@exlex.se>
2021-11-15 18:45:27 +0100

    Add utility script for adding new links/bookmarks

---
+++ scripts/add-link.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+function join_by { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+LINKS_FILE="../src/pages/links/content.ts"
+all_categories=(Art Blog Tech Politics Other)
+
+categories=()
+
+name=$(dmenu -i -p "Name: ")
+if [ -z $name ]; then 
+  return 0
+fi
+
+link=$(dmenu -i -p "Url: ")
+if [ -z $link ]; then 
+  return 0
+fi
+
+ping -w 2 -c 1 ${link} >> /dev/null || {
+  notify-send -u normal "\"${link}\" cannot be reached. Is the link correct?"
+  return 0
+}
+
+while true;
+do
+  if [ "${#categories[@]}" -ne 0 ]; then
+    prompt="Categories (${categories[@]}): "
+  else
+    prompt="Categories "
+  fi
+
+  choice=$(printf "%s\n" "${all_categories[@]}" | dmenu -i -p "${prompt}")
+
+  if [ $choice = "DONE" ]; then
+    break
+  fi  
+
+  if [[ " ${all_categories[*]} " =~ " ${choice} " ]]; then
+    categories+=("'${choice}'")
+    all_categories=( ${all_categories[@]/$choice} )
+
+    if [ "${#categories[@]}" -eq 1 ]; then
+      all_categories+=( "DONE" )
+    fi
+  fi
+done
+
+combined_categories=$(join_by ", " ${categories[@]})
+
+notify-send -u normal "Add link ${name} (${link}) with category ${combined_categories}?"
+
+confirmation=$(echo -e "yes\nno" | dmenu -i -p "Confirm")
+if [ "$confirmation" != "yes" ]; then
+  return 0
+fi
+
+echo "links.push(toLink(" >> $LINKS_FILE
+echo "  '${name}'," >> $LINKS_FILE
+echo "  '${link}'," >> $LINKS_FILE
+echo "  ${combined_categories}" >> $LINKS_FILE
+echo -e "));\n" >> $LINKS_FILE
